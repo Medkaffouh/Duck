6f602e43472bec74745c58db2479ef5747013ec7
Medkaffouh <kaffouhmohamed@gmail.com>
2022-03-31 22:54:07 +0000

    Initial commit

---
+++ app/src/main/java/com/hfad/duck/Chapter5Exercice.java
@@ -0,0 +1,13 @@
+package com.hfad.duck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chapter5Exercice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter5_exercice);
+    }
+}
